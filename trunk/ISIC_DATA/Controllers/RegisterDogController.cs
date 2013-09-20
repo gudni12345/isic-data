74615a4da27e15b5f80a80fdc1fc552137923f7f
arnisig <arnisig@0b894d1f-6d79-4208-f1ac-f649a96d2995>
2013-09-20 17:05:00 +0000

    RegisterDog View

---
+++ trunk/ISIC_DATA/Controllers/RegisterDogController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ISIC_DATA.Models;
+
+namespace ISIC_DATA.Controllers
+{
+    public class RegisterDogController : Controller
+    {
+        public ActionResult Index()
+        {
+            var viewModel = new DogViewModel();
+            return this.View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Index(DogViewModel viewModel)
+        {
+
+            return this.View(viewModel);
+        }
+
+    }
+}
