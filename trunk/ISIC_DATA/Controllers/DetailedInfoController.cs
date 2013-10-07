22e63b11ba14ebae502dfaad757efac1ae3f217e
alexmiceland@gmail.com <alexmiceland@gmail.com@0b894d1f-6d79-4208-f1ac-f649a96d2995>
2013-10-07 18:38:12 +0000

    created DetailedInfoController and Views. HealthAppearance.cshtml was thought to be partial view under Dog/Details.cshtml. - needs review.

---
+++ trunk/ISIC_DATA/Controllers/DetailedInfoController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ISIC_DATA.Models;
+using ISIC_DATA.DataAccess;
+
+namespace ISIC_DATA.Controllers
+{
+    public class DetailedInfoController : Controller
+    {
+        private DogContext db = new DogContext();
+
+        //
+        // GET: /DetailedInfo/
+
+        public ActionResult Index()
+        {
+            return View(db.DetailedInfo.ToList());
+        }
+
+        //
+        // GET: /DetailedInfo/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            DetailedInfo detailedinfo = db.DetailedInfo.Find(id);
+            if (detailedinfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(detailedinfo);
+        }
+
+        //
+        // GET: /DetailedInfo/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /DetailedInfo/Create
+
+        [HttpPost]
+        public ActionResult Create(DetailedInfo detailedinfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DetailedInfo.Add(detailedinfo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(detailedinfo);
+        }
+
+        //
+        // GET: /DetailedInfo/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            DetailedInfo detailedinfo = db.DetailedInfo.Find(id);
+            if (detailedinfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(detailedinfo);
+        }
+
+        //
+        // POST: /DetailedInfo/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(DetailedInfo detailedinfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(detailedinfo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(detailedinfo);
+        }
+
+        //
+        // GET: /DetailedInfo/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            DetailedInfo detailedinfo = db.DetailedInfo.Find(id);
+            if (detailedinfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(detailedinfo);
+        }
+
+        //
+        // POST: /DetailedInfo/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            DetailedInfo detailedinfo = db.DetailedInfo.Find(id);
+            db.DetailedInfo.Remove(detailedinfo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
