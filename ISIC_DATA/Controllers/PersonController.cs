7aa2cb398abe903597dfa3df6765b9089ad63f5d
runarthob09 <runarthob09@gmail.com>
2013-10-12 22:29:44 +0000

    Person Coontroller - Index - details Bootstrap table

--- ISIC_DATA/Controllers/PersonController.cs
+++ ISIC_DATA/Controllers/PersonController.cs
@@ -7,7 +7,7 @@ using System.Web;
 using System.Web.Mvc;
 using ISIC_DATA.Models;
 using ISIC_DATA.DataAccess;
-
+using PagedList;
 namespace ISIC_DATA.Controllers
 {
     public class PersonController : Controller
@@ -17,10 +17,42 @@ namespace ISIC_DATA.Controllers
         //
         // GET: /Person/
 
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
         {
-            var person = db.Person.Include(p => p.Country);
-            return View(person.Take(20).ToList());
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            ViewBag.CurrentFilter = searchString;
+
+            var persons = db.Person.Include(p => p.Country);
+            
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                persons=persons.Where(p=> p.Name.ToUpper().Contains(searchString.ToUpper()));
+            }
+            switch (sortOrder)
+            {
+                case "name_dec":
+                    persons= persons.OrderByDescending(p=> p.Name);
+                    break;
+
+                case "country":
+                    persons = persons.OrderBy(p => p.Country.Name);
+                    break;
+                default:
+                    persons = persons.OrderByDescending (p=> p.Country.Name);
+                    break;
+            }
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            return View(persons.ToPagedList(pageNumber, pageSize));
+           // var person = db.Person.Include(p => p.Country);
+           // return View(person.Take(20).ToList());
         }
 
         //
