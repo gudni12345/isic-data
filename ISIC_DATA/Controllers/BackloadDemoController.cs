18734bd63191cf9351bf2da5d375f2d873ef7296
arnisig <arnisig@gmail.com>
2013-11-01 22:16:02 +0000

    upload test

--- ISIC_DATA/Controllers/BackloadDemoController.cs
+++ ISIC_DATA/Controllers/BackloadDemoController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,6 +14,27 @@ namespace ISIC_DATA.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Test()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult uploadTest()
+        {
+            var file = Request.Files[0];
+
+            if (file != null && file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/Photos/"), fileName);
+                file.SaveAs(path);
+            }
+
+            return RedirectToAction("Test");
         }
     }
 }
