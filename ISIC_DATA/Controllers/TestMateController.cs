4280cb136696132327a6bc136b0770ac7ebb4e2f
arnisig <arnisig@gmail.com>
2013-11-08 20:13:29 +0000

    TestMate - fyrstu fjögur checkin

--- ISIC_DATA/Controllers/TestMateController.cs
+++ ISIC_DATA/Controllers/TestMateController.cs
@@ -27,11 +27,56 @@ namespace ISIC_DATA.Controllers
         [Authorize(Roles = "Administrator,SuperAdministrator")]
         public ActionResult Index(TestMateViewModel viewModel)
         {
-            // We only get Id of the Dog backfrom the viewModel.
-            Dog Father = db.Dog.Find(viewModel.Father.Id);
-            Dog Mother = db.Dog.Find(viewModel.Mother.Id);
-
+            /*Typical coancestries between relatives are as follows:
+              1.  Father/daughter, mother/son or brother/sister → 25% (1⁄4)
+              2.  Grandfather/granddaughter or grandmother/grandson → 12.5% (1⁄8)
+              3.  Half-brother/half-sister, Double cousins → 12.5% (1⁄8)
+              4.  Uncle/niece or aunt/nephew → 12.5% (1⁄8)
+              5.  Great-grandfather/great-granddaughter or great-grandmother/great-grandson → 6.25% (1⁄16)
+              6   Half-uncle/niece or half-aunt/nephew → 6.25% (1⁄16)
+              7.  First cousins → 6.25% (1⁄16)
+             */
+            
             double result = 0.0;
+ 
+            // We only get Id of the Dog backfrom the viewModel.
+            Dog A = db.Dog.Find(viewModel.Father.Id);
+            Dog B = db.Dog.Find(viewModel.Mother.Id);
+
+            // Check for no 1. Father/daughter, mother/son or brother/sister → 25% (1⁄4)
+            if ((A.Id == B.Litter.FatherId) || // Father/daughter
+                (A.Litter.MotherId == B.Id) || // son /mother
+                (A.LitterId == B.LitterId) ||  // Same litter
+                ((A.Litter.FatherId == B.Litter.FatherId) && (A.Litter.MotherId == B.Litter.MotherId))) // or have same parents
+            {
+                result = 25;
+            }
+            else    // Check for no 2.  Grandfather/granddaughter or grandmother/grandson → 12.5% (1⁄8)
+                if ((A.Id == B.Litter.Father.Litter.FatherId) ||   //Grandfather/granddaughter
+                    (A.Litter.Mother.Litter.MotherId == B.Id))      //grandmother/grandson
+                {
+                    result = 12.5;
+                }
+                else //Check for no 3.  Half-brother/half-sister, Double cousins → 12.5% (1⁄8)
+                    if ((A.Litter.FatherId == B.Litter.FatherId) || // Same father or same Mother
+                        (A.Litter.MotherId == B.Litter.MotherId))
+                    {
+                        result = 12.5;
+                    }
+                    else  // Check for no 4.  Uncle/niece or aunt/nephew → 12.5% (1⁄8)   
+                        if ((A.Litter.FatherId == B.Litter.Father.Litter.FatherId) ||    // Uncle/niece 
+                            (A.Litter.FatherId == B.Litter.Mother.Litter.FatherId) ||    // Uncle/niece                             
+                            (A.Litter.MotherId == B.Litter.Father.Litter.MotherId) ||    // Uncle/niece 
+                            (A.Litter.MotherId == B.Litter.Mother.Litter.MotherId) ||    // Uncle/niece 
+                            (A.Litter.Father.Litter.FatherId == B.Litter.FatherId) ||   // aunt/nephew
+                            (A.Litter.Mother.Litter.FatherId == B.Litter.FatherId) ||   // aunt/nephew
+                            (A.Litter.Father.Litter.MotherId == B.Litter.MotherId) ||   // aunt/nephew
+                            (A.Litter.Mother.Litter.MotherId == B.Litter.MotherId))     // aunt/nephew
+                        {
+                            result = 12.5;
+                        }
+            
+            /*
             if (Father.LitterId == Mother.LitterId)   // Systkini í sama goti.
             {
                 result = 0.5;
@@ -46,10 +91,11 @@ namespace ISIC_DATA.Controllers
 
                 else
                     result = 0.0;
+            */
 
             ViewBag.Result = result;
-            viewModel.Father.Name = Father.Name;
-            viewModel.Mother.Name = Mother.Name;
+            viewModel.Father.Name = A.Name;
+            viewModel.Mother.Name = B.Name;
             ModelState.Clear();
             return View(viewModel);
         }
