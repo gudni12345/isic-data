9f65523a984fc454402856247983ab288b4b6af3
alexmiceland@gmail.com <alexmiceland@gmail.com@0b894d1f-6d79-4208-f1ac-f649a96d2995>
2013-11-13 01:40:12 +0000

    Updated TestMate Controller, now result is summed up. Added test cases for n=5

--- trunk/ISIC_DATA/Controllers/TestMateController.cs
+++ trunk/ISIC_DATA/Controllers/TestMateController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ISIC_DATA.Models;
+using System.Diagnostics;
 
 namespace ISIC_DATA.Controllers
 {
@@ -35,106 +36,91 @@ namespace ISIC_DATA.Controllers
                   7.  First cousins → 6.25% (1⁄16)
                  */
 
-            double result = 0.0;
+            double result = 0.0;   // A: Father, B: Mother
 
-            // Check for no 1. Father/daughter, mother/son or brother/sister → 25% (1⁄4)
-            if ((A.Id == B.Litter.FatherId) || // Father/daughter
-                (A.Litter.MotherId == B.Id) || // son /mother
-                (A.LitterId == B.LitterId) ||  // Same litter
-                ((A.Litter.FatherId == B.Litter.FatherId) && (A.Litter.MotherId == B.Litter.MotherId))) // or have same parents
-            {
-                result = 25;
-            }
-            else    // Check for no 2.  Grandfather/granddaughter or grandmother/grandson → 12.5% (1⁄8)
-                if ((A.Id == B.Litter.Father.Litter.FatherId) ||   //Grandfather/granddaughter
-                    (A.Litter.Mother.Litter.MotherId == B.Id))      //grandmother/grandson
-                {
-                    result = 12.5;
-                }
-                else //Check for no 3.  Half-brother/half-sister, Double cousins → 12.5% (1⁄8)
-                    if ((A.Litter.FatherId == B.Litter.FatherId) || // Same father or same Mother
-                        (A.Litter.MotherId == B.Litter.MotherId))
-                    {
-                        result = 12.5;
-                    }
-                    else  // Check for no 4.  Uncle/niece or aunt/nephew → 12.5% (1⁄8)   
-                        if (((A.Litter.FatherId == B.Litter.Father.Litter.FatherId) &&    // Uncle/niece
-                             (A.Litter.MotherId == B.Litter.Father.Litter.MotherId)) ||
-                             ((A.Litter.FatherId == B.Litter.Mother.Litter.FatherId) &&  // Uncle/niece
-                              (A.Litter.MotherId == B.Litter.Mother.Litter.MotherId)) ||
+            // Check for no 1. Father/daughter, mother/son or brother/sister → 25% (1⁄4) 
+            //***Pathmothod: n=2****
+            if        (A.Id == B.Litter.FatherId) {result = 0.25;} // Father/daughter
+            if   (A.Litter.MotherId == B.Id) {result += 0.25;}// son /mother
 
-                            ((A.Litter.Father.Litter.FatherId == B.Litter.FatherId) &&   // aunt/nephew
-                            (A.Litter.Father.Litter.MotherId == B.Litter.MotherId)) ||
-                            ((A.Litter.Mother.Litter.FatherId == B.Litter.FatherId) &&   // aunt/nephew  
-                            (A.Litter.Mother.Litter.MotherId == B.Litter.MotherId)))
-                        {
-                            result = 12.5;
-                        }
-                        else // 5.  Great-grandfather/great-granddaughter or great-grandmother/great-grandson → 6.25% (1⁄16)
-                            if ((A.Id == B.Litter.Father.Litter.Father.Litter.FatherId) || // Great-grandfather/great-granddaughter
-                                (A.Id == B.Litter.Mother.Litter.Father.Litter.FatherId) ||
-                                (A.Id == B.Litter.Father.Litter.Mother.Litter.FatherId) ||
-                                (A.Id == B.Litter.Mother.Litter.Mother.Litter.FatherId) ||
+            // ***(n=3)***
+            if   (A.Litter.FatherId == B.Litter.FatherId) {result += 0.125;} // A and B have same father 
+            if   (A.Litter.MotherId == B.Litter.MotherId) {result += 0.125;} // A and B have same mother
+            
+            // Check for no 2.  Grandfather/granddaughter or grandmother/grandson → 12.5% (1⁄8)
+            if  (A.Id == B.Litter.Father.Litter.FatherId) {result += 0.125;} // A is B´s Grandfather (farfar)
+            if  (A.Id == B.Litter.Mother.Litter.FatherId) {result += 0.125;} // A is B´s Grandfather (morfar)
+            if  (A.Litter.Mother.Litter.MotherId == B.Id) {result += 0.125;} // B is A´s Grandmother (mormor)
+            if  (A.Litter.Father.Litter.MotherId == B.Id) {result += 0.125;} // B is A´s Grandmother (farmor)
+            
+            
+                
+             // Check for no 4.  Uncle/niece or aunt/nephew → 12.5% (1⁄8), if full related, half is 6.25%
+             // ***(n=4)***
+             if (A.Litter.FatherId == B.Litter.Father.Litter.FatherId) {result += 0.0625;} //A´s Father is B´s Grandpa (farfar)
+             if (A.Litter.MotherId == B.Litter.Father.Litter.MotherId) {result += 0.0625;} //A´s Mother is B´s Grandmother (farmor)
+             if (A.Litter.FatherId == B.Litter.Mother.Litter.FatherId) {result += 0.0625;} //A´s Father is B´s Grandpa (morfar)
+             if (A.Litter.MotherId == B.Litter.Mother.Litter.MotherId) {result += 0.0625;} //A´s Mother is B´s Grandmother (farmor)
+             if (A.Litter.Father.Litter.FatherId == B.Litter.FatherId) {result += 0.0625;} //B´s Father is A´s Grandpa (farfar)
+             if (A.Litter.Father.Litter.MotherId == B.Litter.MotherId) {result += 0.0625;} //B´s Mother is A´s Grandmother (farmor)
+             if (A.Litter.Mother.Litter.FatherId == B.Litter.FatherId) {result += 0.0625;} //B´s Father is A´s Grandpa (morfar)
+             if (A.Litter.Mother.Litter.MotherId == B.Litter.MotherId) {result += 0.0625;} //B´s Mother is A´s Grandmother (farmor)
+                       
+             // 5.  Great-grandfather/great-granddaughter or great-grandmother/great-grandson → 6.25% (1⁄16)
+             if (A.Id == B.Litter.Father.Litter.Father.Litter.FatherId) {result += 0.0625;} //A is B´s GreatGrandpa (either mom´s
+             if (A.Id == B.Litter.Mother.Litter.Father.Litter.FatherId) {result += 0.0625;} //                or dad/granddad´s side)
+             if (A.Id == B.Litter.Father.Litter.Mother.Litter.FatherId) {result += 0.0625;}
+             if (A.Id == B.Litter.Mother.Litter.Mother.Litter.FatherId) {result += 0.0625;}
+                 
+             if (A.Litter.Father.Litter.Father.Litter.FatherId == B.Id) {result += 0.0625;} //B is A´s GreatGrandmother (either mom´s
+             if (A.Litter.Mother.Litter.Father.Litter.FatherId == B.Id) {result += 0.0625;} //                or dad´s side)
+             if (A.Litter.Father.Litter.Mother.Litter.FatherId == B.Id) {result += 0.0625;}
+             if (A.Litter.Mother.Litter.Mother.Litter.FatherId == B.Id) {result += 0.0625;}
+                          
+             // 6.  Half-uncle/niece or half-aunt/nephew → 6.25% (1⁄16) --> covered under nr.4
+             
+             // 7.  First cousins → 6.25% (1⁄16) - Syskina börn - sami afi og amma, if two lines under 8. are true
+             // 8.  Half First cousins → 3.125% (1⁄32) - Either grandma og grandpa the same
+             // ***(n=5)***                             
+             if (A.Litter.Father.Litter.FatherId == B.Litter.Father.Litter.FatherId) {result += 0.03125;}  //  Same grandfather                
+             if (A.Litter.Mother.Litter.FatherId == B.Litter.Father.Litter.FatherId) {result += 0.03125;}
+             if (A.Litter.Father.Litter.FatherId == B.Litter.Mother.Litter.FatherId) {result += 0.03125;}
+             if (A.Litter.Mother.Litter.FatherId == B.Litter.Mother.Litter.FatherId) {result += 0.03125;}
 
-                                (A.Litter.Father.Litter.Father.Litter.FatherId == B.Id) ||  // great-grandmother/great-grandson
-                                (A.Litter.Mother.Litter.Father.Litter.FatherId == B.Id) ||
-                                (A.Litter.Father.Litter.Mother.Litter.FatherId == B.Id) ||
-                                (A.Litter.Mother.Litter.Mother.Litter.FatherId == B.Id))
-                            {
-                                result = 6.25;
-                            }
-                            else // 6.  Half-uncle/niece or half-aunt/nephew → 6.25% (1⁄16)
-                                if ((A.Litter.FatherId == B.Litter.Father.Litter.FatherId) ||    // Uncle/niece
-                                     (A.Litter.MotherId == B.Litter.Father.Litter.MotherId) ||
-                                     (A.Litter.FatherId == B.Litter.Mother.Litter.FatherId) ||  // Uncle/niece
-                                     (A.Litter.MotherId == B.Litter.Mother.Litter.MotherId) ||
+             if (A.Litter.Father.Litter.MotherId == B.Litter.Father.Litter.MotherId) {result += 0.03125;}  //  Same grandmother
+             if (A.Litter.Mother.Litter.MotherId == B.Litter.Father.Litter.MotherId) {result += 0.03125;}
+             if (A.Litter.Father.Litter.MotherId == B.Litter.Mother.Litter.MotherId) {result += 0.03125;}
+             if (A.Litter.Mother.Litter.MotherId == B.Litter.Mother.Litter.MotherId) {result += 0.03125;}
+                                        
 
-                                     (A.Litter.Father.Litter.FatherId == B.Litter.FatherId) ||   // aunt/nephew
-                                     (A.Litter.Father.Litter.MotherId == B.Litter.MotherId) ||
-                                     (A.Litter.Mother.Litter.FatherId == B.Litter.FatherId) ||   // aunt/nephew  
-                                     (A.Litter.Mother.Litter.MotherId == B.Litter.MotherId))
-                                {
-                                    result = 6.25;
-                                }
-                                else // 7.  First cousins → 6.25% (1⁄16) - Syskina börn - sami afi og amma
-                                    if (((A.Litter.Father.Litter.FatherId == B.Litter.Father.Litter.FatherId) ||  //  Same grandfather
-                                          (A.Litter.Mother.Litter.FatherId == B.Litter.Father.Litter.FatherId) ||
-                                          (A.Litter.Father.Litter.FatherId == B.Litter.Mother.Litter.FatherId) ||
-                                          (A.Litter.Mother.Litter.FatherId == B.Litter.Mother.Litter.FatherId)) &&
+             // 9.  → 6,25% (1⁄32) - Great Grandma is the mother (Great-uncle/ neice and Great-aunt/ uncle)
+             if (A.Litter.Father.Litter.Father.Litter.MotherId == B.Litter.MotherId) {result += 0.03125;}
+             if (A.Litter.Mother.Litter.Father.Litter.MotherId == B.Litter.MotherId) {result += 0.03125;}
+             if (A.Litter.Father.Litter.Mother.Litter.MotherId == B.Litter.MotherId) {result += 0.03125;}
+             if (A.Litter.Mother.Litter.Mother.Litter.MotherId == B.Litter.MotherId) {result += 0.03125;}
 
-                                          ((A.Litter.Father.Litter.MotherId == B.Litter.Father.Litter.MotherId) ||  //  Same grandmother
-                                          (A.Litter.Mother.Litter.MotherId == B.Litter.Father.Litter.MotherId) ||
-                                          (A.Litter.Father.Litter.MotherId == B.Litter.Mother.Litter.MotherId) ||
-                                          (A.Litter.Mother.Litter.MotherId == B.Litter.Mother.Litter.MotherId)))
-                                    {
-                                        result = 6.25;
-                                    }
-                                    
-                                    
-                                    else // 8.  Half First cousins → 3.125% (1⁄32) - Either grandma og grandpa the same
-                                        if (((A.Litter.Father.Litter.FatherId == B.Litter.Father.Litter.FatherId) ||  //  Same grandfather
-                                              (A.Litter.Mother.Litter.FatherId == B.Litter.Father.Litter.FatherId) ||
-                                              (A.Litter.Father.Litter.FatherId == B.Litter.Mother.Litter.FatherId) ||
-                                              (A.Litter.Mother.Litter.FatherId == B.Litter.Mother.Litter.FatherId)) ||
+             if (A.Litter.Father.Litter.Father.Litter.FatherId == B.Litter.FatherId) {result += 0.03125;}
+             if (A.Litter.Mother.Litter.Father.Litter.FatherId == B.Litter.FatherId) {result += 0.03125;}
+             if (A.Litter.Father.Litter.Mother.Litter.FatherId == B.Litter.FatherId) {result += 0.03125;}
+             if (A.Litter.Mother.Litter.Mother.Litter.FatherId == B.Litter.FatherId) {result += 0.03125;}
 
-                                              ((A.Litter.Father.Litter.MotherId == B.Litter.Father.Litter.MotherId) ||  //  Same grandmother
-                                              (A.Litter.Mother.Litter.MotherId == B.Litter.Father.Litter.MotherId) ||
-                                              (A.Litter.Father.Litter.MotherId == B.Litter.Mother.Litter.MotherId) ||
-                                              (A.Litter.Mother.Litter.MotherId == B.Litter.Mother.Litter.MotherId)))
-                                        {
-                                            result = 3.125;
-                                        }
+             if (B.Litter.Father.Litter.Father.Litter.MotherId == A.Litter.MotherId) {result += 0.03125;}
+             if (B.Litter.Mother.Litter.Father.Litter.MotherId == A.Litter.MotherId) {result += 0.03125;}
+             if (B.Litter.Father.Litter.Mother.Litter.MotherId == A.Litter.MotherId) {result += 0.03125;}
+             if (B.Litter.Mother.Litter.Mother.Litter.MotherId == A.Litter.MotherId) {result += 0.03125;}
 
-                                     else // 9.  → 6,25% (1⁄32) - Great Grandma is the mother (Great-uncle/ neice and Great-aunt/ uncle)
-                                            if (((A.Litter.Father.Litter.Mother.Litter.MotherId == B.Litter.MotherId) ||
-                                                 (A.Litter.Father.Litter.Mother.Litter.FatherId == B.Litter.FatherId)))
-                                            {
-                                                result = 3.125;
-                                            }
+             if (B.Litter.Father.Litter.Father.Litter.FatherId == A.Litter.FatherId) {result += 0.03125;}
+             if (B.Litter.Mother.Litter.Father.Litter.FatherId == A.Litter.FatherId) {result += 0.03125;}
+             if (B.Litter.Father.Litter.Mother.Litter.FatherId == A.Litter.FatherId) {result += 0.03125;}
+             if (B.Litter.Mother.Litter.Mother.Litter.FatherId == A.Litter.FatherId) {result += 0.03125;}
+
+             //10. to be completed n=6
 
 
 
-            return result;
+             
+             result = result*100;
+             return result;
         }
 
 
