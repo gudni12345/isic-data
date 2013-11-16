f7bc5bf82be1883144d84e066c91631a35d0ee4a
arnisig <arnisig@gmail.com>
2013-11-16 14:12:15 +0000

    Setti inn Repository fyrir UnitTest Einnig breytingar á DogController sem notar núna Repository.

---
+++ ISIC_DATA/DataAccess/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using ISIC_DATA.Models;
+using System;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ISIC_DATA.DataAccess
+{
+    public interface IRepository<T>
+    {
+    //    IQueryable<Dog> FindAllDogs();
+   //     void Add(Dog dog);
+   //     void Delete(Dog dog);
+    //    void Save();
+        IQueryable<T> All { get; }
+        IQueryable<T> AllIncluding(params Expression<Func<T, object>>[] includeProperties);
+        Dog Find(int id);
+        DbEntityEntry Entry(object o);
+        void InsertOrUpdate(T dog);
+        void Dispose();
+        int SaveChanges();
+    }
+}
