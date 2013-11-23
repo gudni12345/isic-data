2fad9947832b413d7b84081deea5510ab9b0e630
alexmiceland@gmail.com <alexmiceland@gmail.com@0b894d1f-6d79-4208-f1ac-f649a96d2995>
2013-11-23 21:06:50 +0000

    added logic to testmate - now it shows ID, needs to be changed to name

---
+++ trunk/ISIC_DATA/Lib/InbreedingResult.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ISIC_DATA.Lib
+{
+
+    public class InbreedingResult
+    {
+        public InbreedingResult()
+        {
+
+        }
+
+        public double Result
+        {
+            get;
+            set;
+        }
+
+        public int CommonAncestorID
+        {
+            get;
+            set;
+
+        }
+
+        public string CommonAncestorName
+        {
+            get;
+            set;
+        }
+    }
+
+}
