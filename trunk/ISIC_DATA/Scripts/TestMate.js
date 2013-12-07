8a1115ecbc5215ad9fa8b103ad54a64375e4dc56
arnisig@gmail.com <arnisig@gmail.com@0b894d1f-6d79-4208-f1ac-f649a96d2995>
2013-12-07 13:59:18 +0000

    Testmate.js

---
+++ trunk/ISIC_DATA/Scripts/TestMate.js
@@ -0,0 +1,75 @@
+﻿  
+    $(document).ready(function () {
+        $("#typeaheadFather").typeahead({
+            name: "Father", 
+            remote: 'Registerdog/FetchFathers?q=%QUERY',
+            limit: 3,
+            valueKey: "Reg",
+            template: ['<p class="repo-reg">{{Reg}}</p>', '<p class="repo-name">{{Name}}</p>',
+            ].join(''),
+            engine: Hogan
+        }).on("typeahead:selected typeahead:autocompleted",
+            function (e, datum) {
+                $('#returnFatherId').val(datum.Id);
+                $('#fatherName').val(datum.Name);
+
+            }
+        );
+    });
+
+$(document).ready(function () {
+    $("#typeaheadMother").typeahead({
+        name: "Mother",
+        remote: 'Registerdog/FetchMothers?q=%QUERY',
+        limit: 3,
+        valueKey: "Reg",
+        template: ['<p class="repo-reg">{{Reg}}</p>', '<p class="repo-name">{{Name}}</p>',].join(''),
+        engine: Hogan
+    }).on("typeahead:selected typeahead:autocompleted",
+        function (e, datum) {
+            $('#returnMotherId').val(datum.Id);
+            $('#motherName').val(datum.Name);
+
+        }
+    );
+});
+
+
+//MODAL for Inbreeding info
+$(function () {
+    $.ajaxSetup({ cache: false });
+    $('#info').click(function () {                 // Open the Modal
+        $('#dialogContent').load(this.href, function () {
+            $('#dialogDiv').modal({
+                backdrop: 'static',
+                keyboard: true
+            }, 'show');
+            bindForm(this);
+        });
+        return false;
+    });
+});
+
+function bindForm(dialog) {
+    $('form', dialog).submit(function () {
+        $.ajax({
+            url: this.action,
+            type: this.method,
+            data: $(this).serialize(),
+            success: function (result) {
+                if (result.success) {           // if data was saved successfully we close the modal.
+                    $('#dialogDiv').modal('hide');
+                    //Refresh: location.reload();
+
+                } else {
+                    $('#dialogContent').html(result);
+                    bindForm();
+                }
+            }
+        });
+        return false;
+    });
+}
+
+
+
