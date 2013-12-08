b72218bf6ec9533bedcf9655eafc893d7bb11cee
arnisig <arnisig@gmail.com>
2013-12-08 19:02:20 +0000

    Unittest uppfærsla

--- UnitTest_ISIC_DATA/Fakes/FakePedigree.cs
+++ UnitTest_ISIC_DATA/Fakes/FakePedigree.cs
@@ -13,20 +13,18 @@ namespace UnitTest_ISIC_DATA.Fakes
         {
             List<Dog> dogs = new List<Dog>();
 
-            Dog rakki = new Dog() { Id = 1, Name = "Rakki_Founder", Reg = "IS1", Sex = "M", LitterId = 0 };
-            Dog tik = new Dog() { Id = 2, Name = "Tík_Founder", Reg = "IS2", Sex = "M", LitterId = 0 };
-            Dog sampleDog = new Dog() { Id=3, Name = "Rakki", Reg = "IS3", Sex = "M", Father = new List<Litter>(), Mother = new List<Litter>()};
-
-            Litter litter = new Litter() { Id = 1, FatherId = 1, MotherId = 2 };
-            sampleDog.LitterId = litter.Id;
-
-            sampleDog.Father.Add(litter);
-            sampleDog.Mother.Add(litter);
-            sampleDog.LitterId = 1;
-
+            Person person = new Person() { Id = 1, Name = "Jón Jónsson" };
+            Dog rakki = new Dog() { Id = 1, Name = "Rakki_Founder", Reg = "IS1", Sex = "M", LitterId = 1 };
+            Dog tik = new Dog() { Id = 2, Name = "Tík_Founder", Reg = "IS2", Sex = "M", LitterId = 1 };
             dogs.Add(rakki);
             dogs.Add(tik);
-            dogs.Add(sampleDog);
+
+            Litter litter1 = new Litter() { Id = 1, FatherId = 1, MotherId = 2, PersonId = 1 };
+            Litter litter2 = new Litter() { Id = 2, FatherId = 1, MotherId = 2, PersonId = 1 };  
+            Dog hvolpur1 = new Dog() { Id = 3, Name = "Hvolpur1", Reg = "IS3", Sex = "M", LitterId = 2 };
+            Dog hvolpur2 = new Dog() { Id = 4, Name = "Hvolpur2", Reg = "IS4", Sex = "F", LitterId = 2 };
+            dogs.Add(hvolpur1);
+            dogs.Add(hvolpur2);
 
             return dogs;
         }
